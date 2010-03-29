bdd7ffc4b0514292e47505a529d2f8808ef8ab95
Seraphim Mellos <mellos@indifex.com>
2010-03-29 14:38:55 +0300

    Added missing files from previous commit

---
+++ hcore/templatetags/dict_parser.py
@@ -0,0 +1,12 @@
+from django import template
+register = template.Library()
+
+def dict_get(value, arg):
+    #custom template tag used like so:
+    #{{dictionary|dict_get:var}}
+    #where dictionary is duh a dictionary and var is a variable representing
+    #one of it's keys
+
+    return value.get(arg, None)
+
+register.filter('dict_get',dict_get)
