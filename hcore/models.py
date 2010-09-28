1295bc5857f5a5688556c68d1dffb37d957b99aa
Stefanos Kozanis <S.Kozanis@itia.ntua.gr>
2010-09-28 09:43:40 +0300

    Start and end dates from SQL stored procedures, tickted #153

--- hcore/models.py
+++ hcore/models.py
@@ -490,12 +490,7 @@ class Timeseries(models.Model):
             return None
         # This should be removed if ticket #112 gets resolved
         c = db_connection.cursor()
-        c.execute(
-            "SELECT to_timestamp(substring("
-            " CASE WHEN top='' OR top IS NULL THEN bottom ELSE top END from"
-            " '^(.*?),'), 'YYYY-MM-DD HH24:MI')::timestamp FROM ts_records"
-            " WHERE id=%s" , (ts.id,)
-        )
+        c.execute("SELECT timeseries_start_date(%s)", (ts.id,))
         try:
             r = c.fetchone()
         except:
@@ -511,11 +506,7 @@ class Timeseries(models.Model):
             return None
         # This should be removed if ticket #112 gets resolved
         c = db_connection.cursor()
-        c.execute(
-            "SELECT to_timestamp(substring(bottom from E'\n([^,]*?),[^\n]*?\n?$'),"
-            " 'YYYY-MM-DD HH24:MI')::timestamp FROM ts_records"
-            " WHERE id=%s" , (ts.id,)
-        )
+        c.execute("SELECT timeseries_end_date(%s)", (ts.id,))
         try:
             r = c.fetchone()
         except:
