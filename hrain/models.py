b178731079cba40dd881a5b3b12975b3be42277e
Antonis Christofides <anthony@itia.ntua.gr>
2010-12-10 16:46:17 +0200

    More hrain development

---
+++ hrain/models.py
@@ -0,0 +1,25 @@
+from django.db import models, connection
+import settings
+
+
+class Event(models.Model):
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+    def __unicode__(self):
+        return "%s - %s" % (start_date.isoformat(), end_date.isoformat())
+
+
+def refresh_events():
+    from pthelma.timeseries import Timeseries, identify_events
+    ts_list = [Timeseries(id=x) for x in settings.HRAIN_TIMESERIES]
+    for x in ts_list:
+        x.read_from_db(connection) 
+    events = identify_events(ts_list, settings.HRAIN_START_THRESHOLD,
+        settings.HRAIN_NTIMESERIES_START_THRESHOLD,
+        settings.HRAIN_TIME_SEPARATOR,
+        settings.HRAIN_END_THRESHOLD, settings.HRAIN_NTIMESERIES_END_THRESHOLD)
+    Event.objects.all().delete()
+    for event in events:
+        e = Event(start_date=event[0], end_date=event[1])
+        e.save()
--- hrain/views.py
+++ hrain/views.py
@@ -1,15 +1,6 @@
-import settings
-from django.db import connection
 from django.shortcuts import render_to_response
-
-from pthelma.timeseries import Timeseries, identify_events
+from enhydris.hrain import models
 
 def index(request):
-    ts_list = [Timeseries(id=x) for x in settings.HRAIN_TIMESERIES]
-    for x in ts_list:
-        x.read_from_db(connection) 
-    events = identify_events(ts_list, settings.HRAIN_START_THRESHOLD,
-        settings.HRAIN_NTIMESERIES_START_THRESHOLD,
-        settings.HRAIN_TIME_SEPARATOR,
-        settings.HRAIN_END_THRESHOLD, settings.HRAIN_NTIMESERIES_END_THRESHOLD)
-    return render_to_response('rain-index.html', { 'events': events })
+    return render_to_response('rain-index.html',
+        { 'events': models.Event.objects.all() })
