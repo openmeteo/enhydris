d4b367e9827a4aa14d89f322d73a7dabe217549e
Stefanos Kozanis <S.Kozanis@itia.ntua.gr>
2011-03-19 01:05:16 +0200

    Working on chart pages

--- hchartpages/views.py
+++ hchartpages/views.py
@@ -1,11 +1,14 @@
 from django.views.generic import list_detail
 from django.shortcuts import get_object_or_404
-from enhydris.hchartpages.models import ChartPage
+from enhydris.hchartpages.models import ChartPage, Chart, Variable
 
 def chartpage_detail(request, urlcode, **kwargs):
     kwargs["queryset"] = ChartPage.objects.all()
     page = get_object_or_404(ChartPage, url_name = urlcode)
     object_id = page.id
+    charts = Chart.objects.order_by('order').filter(chart_page=object_id)
+    variables = Variable.objects.order_by('chart__order').filter(chart__chart_page=object_id)
     kwargs["template_name"] = "chart_detail.html"
     kwargs["template_object_name"] = "page"
+    kwargs["extra_context"] = { 'charts': charts, 'variables': variables}
     return list_detail.object_detail(request, object_id = object_id, **kwargs)
