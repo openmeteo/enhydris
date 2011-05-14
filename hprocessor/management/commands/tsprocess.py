3caa75c17859fe4bb4a631a75c0114f69f7851a9
Stefanos Kozanis <S.Kozanis@itia.ntua.gr>
2011-05-14 11:24:58 +0300

    Add code for multi timeseries calcs in hprocessor app

--- hprocessor/management/commands/tsprocess.py
+++ hprocessor/management/commands/tsprocess.py
@@ -10,9 +10,40 @@ from pthelma.tsprocess import (MultiTimeseriesProcessDb,
 from django.conf import settings
 
 
+def get_cache_dir():
+    if hasattr(settings, 'TS_GRAPH_CACHE_DIR'):
+        return settings.TS_GRAPH_CACHE_DIR
+    else:
+        return '/var/tmp/enhydris-timeseries/'
+
+
+def clear_timeseries_cache(timeseries_id):
+    afilename = os.path.join(get_cache_dir(),
+                             '%d.hts'%int(timeseries_id))
+    if os.path.exists(afilename):
+        os.remove(afilename)
+
+
+@db.transaction.commit_on_success
+def multi_ts_process(job):
+    job_vars = ProcessInputVariable.objects.filter(process_unit__id=job.id)
+    timeseries_arg = {}
+    for job_var in job_vars:
+        timeseries_arg[job_var.spec] = job_var.timeseries.id
+    options = {}
+    if job.append_only:
+        options['append_only']=True
+    MultiTimeseriesProcessDb(method=job.method, timeseries_arg=timeseries_arg, 
+                             out_timeseries_id=job.output_timeseries.id,
+                             db=db.connection, transaction=db.transaction,
+                             options=options)
+    if not job.append_only:
+        clear_timeseries_cache(job.output_timeseries.id)
+
+
 @db.transaction.commit_on_success
 def ts_aggregation(job):
-    job_var = ProcessInputVariable.objects.all()[0]
+    job_var = ProcessInputVariable.objects.filter(process_unit__id=job.id)[0]
     AggregateDbTimeseries(source_id=job_var.timeseries.id, 
                           dest_id=job.output_timeseries.id, 
                           db=db.connection, transaction=db.transaction,
@@ -21,22 +52,11 @@ def ts_aggregation(job):
                           missing_flag=job.aggregation_missing_flag,
                           append_only=job.append_only)
     if not job.append_only:
-        if hasattr(settings, 'TS_GRAPH_CACHE_DIR'):
-            cache_dir = settings.TS_GRAPH_CACHE_DIR
-        else:
-            cache_dir = '/var/tmp/enhydris-timeseries/'
-        afilename = os.path.join(cache_dir, 
-                                 '%d.hts'%int(job.output_timeseries.id))
-        if os.path.exists(afilename):
-            os.remove(afilename)
+        clear_timeseries_cache(job.output_timeseries.id)
 
 
 def ts_cache_update(id):
-    if hasattr(settings, 'TS_GRAPH_CACHE_DIR'):
-        cache_dir = settings.TS_GRAPH_CACHE_DIR
-    else:
-        cache_dir = '/var/tmp/enhydris-timeseries/'
-    update_ts_temp_file(cache_dir, db.connection, id)
+    update_ts_temp_file(get_cache_dir(), db.connection, id)
 
 
 def process_batch(batch):
@@ -45,7 +65,7 @@ def process_batch(batch):
     for job in jobs:
         if job.method in ('HeatIndex', 'SSI', 'IDM_monthly',
                           'IDM_annual', ):
-            pass
+            multi_ts_process(job)
         elif job.method == 'Aggregation':
             ts_aggregation(job)
         elif job.method == 'Tsupdate':
@@ -68,5 +88,3 @@ class Command(BaseCommand):
                 process_batch(batch)
             except ProcessBatch.DoesNotExist:
                 raise CommandError('Batch "%s" does not exist' %(batch_name,))
-
-
