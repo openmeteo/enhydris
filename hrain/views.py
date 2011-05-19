a6f1a7ec56a67e0b6c1f1920c2cc71e430e90567
Stefanos Kozanis <S.Kozanis@itia.ntua.gr>
2011-05-19 09:58:57 +0300

    Use thelma.cplot in hrain

--- hrain/views.py
+++ hrain/views.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from django.http import Http404, HttpResponse
 
 from pthelma.timeseries import Timeseries
+from pthelma.cplot import plot_contours
 from enhydris.hrain import models
 
 import matplotlib
@@ -59,66 +60,34 @@ def _create_contour_map(ev):
         (x, y) = gp.point.transform(settings.HRAIN_CONTOUR_SRID, clone=True)
         a.append((x, y, tsev.total_precipitation, gp.name))
 
-    # Get the grid parameters
-    GRANULARITY=100
     (x0, y0, x1, y1) = [float(x) for x in settings.HRAIN_CONTOUR_CHART_BOUNDS]
-    dx, dy = (x1-x0)/GRANULARITY, (y1-y0)/GRANULARITY
-    width, height = x1-x0, y1-y0
-
-    # If we just interpolate, then the contours can be bad. What we actually
-    # need to do is to tell the interpolator that far away the rainfall becomes
-    # zero. So we actually work on a grid that is larger (three times larger),
-    # we define rainfall at the edges as zero, we interpolate in there and
-    # calculate the contours, and we crop it so that only its middle part is
-    # shown. We call this grid the "greater" grid, so we suffix variables with
-    # "gr" when they refer to it.
-    xgr0, ygr0, xgr1, ygr1 = (x0-width, y0-height, x1+width, y1+height)
-    a.append((xgr0, ygr0, 0.0, 'virtual'))
-    a.append((xgr1, ygr0, 0.0, 'virtual'))
-    a.append((xgr0, ygr1, 0.0, 'virtual'))
-    a.append((xgr1, ygr1, 0.0, 'virtual'))
-    a.append((xgr0+width/2, ygr0, 0.0, 'virtual'))
-    a.append((xgr0+width/2, ygr1, 0.0, 'virtual'))
-    a.append((xgr0, ygr0+height/2, 0.0, 'virtual'))
-    a.append((xgr1, ygr0+height/2, 0.0, 'virtual'))
-
-    # Now interpolate
-    x = [i[0] for i in a]
-    y = [i[1] for i in a]
-    z = [i[2] for i in a]
-    rbfi = Rbf(x, y, z, function='linear')
-    xx = np.arange(xgr0, xgr1+dx, dx)
-    yy = np.arange(ygr0, ygr1+dy, dy)
-    zz = np.empty([3*GRANULARITY+1, 3*GRANULARITY+1])
-    for i in xrange(0, 3*GRANULARITY):
-        for j in xrange(0, 3*GRANULARITY):
-            zz[j][i] = rbfi(xx[i], yy[j])
-    # Crop
-    zz = zz[GRANULARITY:2*GRANULARITY+1, GRANULARITY:2*GRANULARITY+1]
-
-    # Create the chart
-    chart_large_dimension = 400 # pixels
-    chart_small_dimension = chart_large_dimension*min(y1-y0, x1-x0)/max(
-                                                                y1-y0, x1-x0)
-    if x1-x0>y1-y0:
-        x_dim, y_dim = chart_large_dimension, chart_small_dimension
-    else:
-        x_dim, y_dim = chart_small_dimension, chart_large_dimension
-    fig = plt.figure(figsize=(x_dim/96.0, y_dim/96.0), dpi=96)
-    ax = plt.axes([0.0, 0.0, 1.0, 1.0]) # Axes should occupy no space
-    ax.set_xticks([])
-    ax.set_yticks([])
-    plt.axis('off')
-    for x, y, z, name in a:
-        if name=='virtual': continue
-        plt.plot(x, y, marker='x', linestyle='None', color='black')
-        plt.text(x, y, name, color='black')
-    im = plt.imshow(zz, interpolation='bilinear', origin='lower',
-        cmap=cm.winter_r, extent=(x0, x1, y0, y1))
-    cs = plt.contour(zz, extent=(x0, x1, y0, y1), colors="red")
-    plt.clabel(cs, inline=1, fontsize=10, fmt="%1.0f")
-
-    fig.savefig(filename)
+    (backgrounds_path, background_filename, mask_filename) = (None,
+                                                              None, None)
+    if hasattr(settings, HRAIN_BACKGROUNDS_PATH):
+        backgrounds_path = settings.HRAIN_BACKGROUNDS_PATH
+        if hasattr(settings, HRAIN_BACKGROUND_IMAGE):
+            background_filename = os.path.join(backgrounds_path,
+                                               HRAIN_BACKGROUND_IMAGE)
+        if hasattr(settings, HRAIN_MASK_IMAGE):
+            mask_filename = os.path.join(backgrounds_path,
+                                         HRAIN_MASK_IMAGE)
+    options ={'contours_font_size': 10, 'labels_format': '%1.0f',
+              'draw_contours': True, 'color_map': 'winter_r',
+              'labels_font_size': 10, 'text_color': 'black',
+              'contours_color': 'red', 'draw_labels': True,
+              'markers_color': 'black', 'markers_style': '+',
+              'draw_markers': True, 'granularity': 100,
+              'chart_bounds_bl_x': x0, 'chart_bounds_bl_y': y0,
+              'chart_bounds_tr_x': x1, 'chart_bounds_tr_y': y1,
+              'chart_bounds_srid': settings.HRAIN_CONTOUR_SRID,
+              'compose_background': (background_filename is not None),
+              'background_image': background_filename,
+              'mask_image': mask_filename,
+              'compose_method': ('composite' if mask_filename else 'multiply'),
+              'swap_bg_fg': False, 'compose_alpha': 0.5, 
+              'compose_offset': 0, 'chart_large_dimension': 400,
+              'backgrounds_path': backgrounds_path }
+    plot_contours(filename, a, options)
 
 
 def _create_chart(tsev):
