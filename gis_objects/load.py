6b82f4a9c8d58075a619c88914b222bb250ca7ea
Stefanos Kozanis <S.Kozanis@itia.ntua.gr>
2011-10-14 23:10:09 +0300

    Initial fixes for gis_objects app

--- gis_objects/load.py
+++ gis_objects/load.py
@@ -1,9 +1,9 @@
 import os
 from django.contrib.gis.utils import LayerMapping
 from django.contrib.gis.gdal import SpatialReference
-from other_gis.models import (GISBorehole, GISPump, GISRefinary,
-                              GISSpring, GISAqueductNode,
-                              GISAqueductLine)
+from enhydris.gis_objects.models import (GISBorehole, GISPump, GISRefinery,
+                                         GISSpring, GISAqueductNode,
+                                         GISAqueductLine, GISReservoir)
 
 site_mapping_borehole = {
     'gis_id'        : 'ID',
@@ -16,11 +16,11 @@ site_mapping_borehole = {
 site_mapping_pump = {
     'gis_id'        : 'ID',
     'name'          : 'NAME',
-    'type'          : 'TYPE',
+    'entity_type'   : 'TYPE',
     'point'         : 'POINT',
 }
 
-site_mapping_refinary = {
+site_mapping_refinery = {
     'gis_id'        : 'ID',
     'name'          : 'NAME',
     'point'         : 'POINT',
@@ -28,14 +28,14 @@ site_mapping_refinary = {
 
 site_mapping_spring = {
     'name'          : 'NAME',
-    'gentity_id'    : 'GENTITY_ID',
+    'original_gentity_id'    : 'GENTITY_ID',
     'point'         : 'POINT',
 }
 
 site_mapping_aqueduct_node = {
     'gis_id'        : 'ID',
     'name'          : 'NAM',
-    'node_type'     : 'NODE_TYPE',
+    'entity_type'     : 'NODE_TYPE',
     'type_name'     : 'Type_Name',
     'point'         : 'POINT',
 }
@@ -43,34 +43,45 @@ site_mapping_aqueduct_node = {
 site_mapping_aqueduct_line = {
     'gis_id'        : 'ID',
     'name'          : 'NAME',
-    'type'          : 'TYPE',
+    'entity_type'   : 'TYPE',
     'q'             : 'Q',
     'exs'           : 'EXS',
     'remarks'       : 'REMARKS',
-    'gentity_id'    : 'GENTITY_ID',
+    'original_gentity_id'    : 'GENTITY_ID',
     'type_name'     : 'Type_Name',
     'linestring'    : 'LINESTRING',
 }
 
+site_mapping_reservoir = {
+    'area'          : 'AREA',
+    'entity_type'   : 'TYPE',
+    'name'          : 'NAME',
+    'original_gentity_id':  'GENTITY_ID',
+    'mpoly'         : 'POLYGON',
+}
+
 borehole_shp = os.path.abspath('/home/itia/soulman/eydap-geodata/borehole.shp')
 pump_shp = os.path.abspath('/home/itia/soulman/eydap-geodata/pumping.shp')
-refinary_shp = os.path.abspath('/home/itia/soulman/eydap-geodata/refinery.shp')
+refinery_shp = os.path.abspath('/home/itia/soulman/eydap-geodata/refinery.shp')
 spring_shp = os.path.abspath('/home/itia/soulman/eydap-geodata/spring.shp')
 aqueduct_node_shp = os.path.abspath('/home/itia/soulman/eydap-geodata/aqueductc.shp')
 aqueduct_line_shp = os.path.abspath('/home/itia/soulman/eydap-geodata/aqueductl.shp')
+reservoir_shp = os.path.abspath('/home/itia/soulman/eydap-geodata/reservoir.shp')
 
 elements_orig = [
             [GISBorehole, site_mapping_borehole, borehole_shp],
             [GISPump, site_mapping_pump, pump_shp],
-            [GISRefinary, site_mapping_refinary, refinary_shp],
+            [GISRefinery, site_mapping_refinery, refinery_shp],
             [GISSpring, site_mapping_spring, spring_shp],
             [GISAqueductNode, site_mapping_aqueduct_node, aqueduct_node_shp],
             [GISAqueductLine, site_mapping_aqueduct_line, aqueduct_line_shp],
+            [GISReservoir, site_mapping_reservoir, reservoir_shp],
            ]
 
-elements = [
-            [GISAqueductLine, site_mapping_aqueduct_line, aqueduct_line_shp],
-           ]
+#elements = [
+#            [GISAqueductLine, site_mapping_aqueduct_line, aqueduct_line_shp],
+#           ]
+elements=elements_orig
 
 #srid=2100 for GGRS87
 
