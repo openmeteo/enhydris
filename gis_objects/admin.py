d4b381555b06a7b669f26064c21c8dcdbf465427
Stefanos Kozanis <S.Kozanis@itia.ntua.gr>
2011-10-14 23:13:30 +0300

    Initial fixes (2) for gis_objects app

--- gis_objects/admin.py
+++ gis_objects/admin.py
@@ -1,10 +1,11 @@
-from other_gis.models import *
+from gis_objects.models import *
 from django.contrib import admin
 
 admin.site.register(GISGentityType, admin.ModelAdmin)
 admin.site.register(GISBorehole, admin.ModelAdmin)
 admin.site.register(GISPump, admin.ModelAdmin)
 admin.site.register(GISSpring, admin.ModelAdmin)
-admin.site.register(GISRefinary, admin.ModelAdmin)
+admin.site.register(GISRefinery, admin.ModelAdmin)
 admin.site.register(GISAqueductNode, admin.ModelAdmin)
 admin.site.register(GISAqueductLine, admin.ModelAdmin)
+admin.site.register(GISReservoir, admin.ModelAdmin)
