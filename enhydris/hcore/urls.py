eea108b3fe32c55ab553357899ae595dccd34302
Antonis Christofides <antonis@wikical.com>
2013-08-09 19:39:16 +0300

    Fixed some wrong statements in urls.py

--- enhydris/hcore/urls.py
+++ enhydris/hcore/urls.py
@@ -1,20 +1,22 @@
 from django.conf import settings
-from django.conf.urls.defaults import patterns
+from django.conf.urls import patterns, url
 from django.views.generic.detail import DetailView
 
 from enhydris.hcore import views
-from enhydris.hcore.models import (Instrument, Timeseries)
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
+
     (r'^$', views.index, {}, 'index'),
 
-    (r'^stations/l/$',
-     views.StationListView.as_view(), name='station_list'),
+    url(r'^stations/l/$', views.StationListView.as_view(),
+        name='station_list'),
 
-    (r'^stations/d/(?P<pk>\d+)/$',
-     views.StationDetailView.as_view(), name='station_detail'),
+    url(r'^stations/d/(?P<pk>\d+)/$', views.StationDetailView.as_view(),
+        name='station_detail'),
 
-    (r'^stations/b/(?P<pk>\d+)/$', DetailView.as_view(), name='station_brief'),
+    url(r'^stations/b/(?P<pk>\d+)/$', DetailView.as_view(),
+        name='station_brief'),
 
     (r'^map/$',
         views.map_view, {}, 'map_view'),
@@ -25,11 +27,11 @@ urlpatterns = patterns('',
     (r'^get_subdivision/(?P<division_id>\d+)/$',
      views.get_subdivision, {}, 'get_subdivision'),
 
-    (r'^instruments/d/(?P<object_id>\d+)/$',
-     views.InstrumentDetailView.as_view(), name='instrument_detail'),
+    url(r'^instruments/d/(?P<object_id>\d+)/$',
+        views.InstrumentDetailView.as_view(), name='instrument_detail'),
 
-    (r'^timeseries/d/(?P<pk>\d+)/$',
-     views.TimeseriesDetailView.as_view(), name='timeseries_detail'),
+    url(r'^timeseries/d/(?P<pk>\d+)/$', views.TimeseriesDetailView.as_view(),
+        name='timeseries_detail'),
 
     (r'^timeseries/data/$',
      views.timeseries_data, {}, 'timeseries_data'),
@@ -46,92 +48,95 @@ urlpatterns = patterns('',
     (r'^gentitygenericdata/(?P<gg_id>\d+)/download/$',
      views.download_gentitygenericdata, {}, 'gentitygenericdata_dl'),
 
-    (r'^site_media/'+settings.GENTITYFILE_DIR+'/.*$',
+    (r'^site_media/' + settings.GENTITYFILE_DIR + '/.*$',
      views.protect_gentityfile, {}, ''),
 
     (r'^(?P<layer>[^/]+)/kml/$',
-     views.kml, {}), 
+     views.kml, {}),
 
     (r'^bound/$', views.bound, {}),
 )
 
 # If users can modify content, enable these views
 if settings.USERS_CAN_ADD_CONTENT:
-    urlpatterns += patterns('',
-    (r'^stations/edit/(?P<station_id>\d+)/$',
-     views.station_edit, {} , 'station_edit'),
+    urlpatterns += patterns(
+        '',
 
-    (r'^stations/add/$',
-     views.station_add, {}, 'station_add'),
+        (r'^stations/edit/(?P<station_id>\d+)/$',
+        views.station_edit, {}, 'station_edit'),
 
-    (r'^stations/delete/(?P<station_id>\d+)/$',
-     views.station_delete, {} , 'station_delete'),
+        (r'^stations/add/$',
+        views.station_add, {}, 'station_add'),
 
-    (r'^timeseries/edit/(?P<timeseries_id>\d+)/$',
-     views.timeseries_edit, {} , 'timeseries_edit'),
+        (r'^stations/delete/(?P<station_id>\d+)/$',
+        views.station_delete, {}, 'station_delete'),
 
-    (r'^timeseries/add/$', views.timeseries_add, {}, 'timeseries_add'),
+        (r'^timeseries/edit/(?P<timeseries_id>\d+)/$',
+        views.timeseries_edit, {}, 'timeseries_edit'),
 
-    (r'^timeseries/delete/(?P<timeseries_id>\d+)/$',
-     views.timeseries_delete, {} , 'timeseries_delete'),
+        (r'^timeseries/add/$', views.timeseries_add, {}, 'timeseries_add'),
 
-    (r'^gentityfile/edit/(?P<gentityfile_id>\d+)/$',
-     views.gentityfile_edit, {} , 'gentityfile_edit'),
+        (r'^timeseries/delete/(?P<timeseries_id>\d+)/$',
+        views.timeseries_delete, {}, 'timeseries_delete'),
 
-    (r'^gentitygenericdata/edit/(?P<ggenericdata_id>\d+)/$',
-     views.gentitygenericdata_edit, {} , 'gentitygenericdata_edit'),
+        (r'^gentityfile/edit/(?P<gentityfile_id>\d+)/$',
+        views.gentityfile_edit, {}, 'gentityfile_edit'),
 
-    (r'^gentityfile/add/$', views.gentityfile_add, {}, 'gentityfile_add'),
-    
-    (r'^gentitygenericdata/add/$', views.gentitygenericdata_add, {}, 'gentitygenericdata_add'),
+        (r'^gentitygenericdata/edit/(?P<ggenericdata_id>\d+)/$',
+        views.gentitygenericdata_edit, {}, 'gentitygenericdata_edit'),
 
-    (r'^gentityfile/delete/(?P<gentityfile_id>\d+)/$',
-     views.gentityfile_delete, {} , 'gentityfile_delete'),
+        (r'^gentityfile/add/$', views.gentityfile_add, {}, 'gentityfile_add'),
 
-    (r'^gentitygenericdata/delete/(?P<ggenericdata_id>\d+)/$',
-     views.gentitygenericdata_delete, {} , 'gentitygenericdata_delete'),
+        (r'^gentitygenericdata/add/$', views.gentitygenericdata_add, {},
+         'gentitygenericdata_add'),
 
-    (r'^gentityevent/edit/(?P<gentityevent_id>\d+)/$',
-     views.gentityevent_edit, {} , 'gentityevent_edit'),
+        (r'^gentityfile/delete/(?P<gentityfile_id>\d+)/$',
+        views.gentityfile_delete, {}, 'gentityfile_delete'),
 
-    (r'^gentityevent/add/$', views.gentityevent_add, {}, 'gentityevent_add'),
+        (r'^gentitygenericdata/delete/(?P<ggenericdata_id>\d+)/$',
+        views.gentitygenericdata_delete, {}, 'gentitygenericdata_delete'),
 
-    (r'^gentityevent/delete/(?P<gentityevent_id>\d+)/$',
-     views.gentityevent_delete, {} , 'gentityevent_delete'),
+        (r'^gentityevent/edit/(?P<gentityevent_id>\d+)/$',
+        views.gentityevent_edit, {}, 'gentityevent_edit'),
 
-    (r'^gentityaltcode/edit/(?P<gentityaltcode_id>\d+)/$',
-     views.gentityaltcode_edit, {} , 'gentityaltcode_edit'),
+        (r'^gentityevent/add/$', views.gentityevent_add, {},
+        'gentityevent_add'),
 
-    (r'^gentityaltcode/add/$', views.gentityaltcode_add, {}, 'gentityaltcode_add'),
+        (r'^gentityevent/delete/(?P<gentityevent_id>\d+)/$',
+        views.gentityevent_delete, {}, 'gentityevent_delete'),
 
-    (r'^gentityaltcode/delete/(?P<gentityaltcode_id>\d+)/$',
-     views.gentityaltcode_delete, {} , 'gentityaltcode_delete'),
+        (r'^gentityaltcode/edit/(?P<gentityaltcode_id>\d+)/$',
+        views.gentityaltcode_edit, {}, 'gentityaltcode_edit'),
 
-    (r'^overseer/edit/(?P<overseer_id>\d+)/$',
-     views.overseer_edit, {} , 'overseer_edit'),
+        (r'^gentityaltcode/add/$', views.gentityaltcode_add, {},
+        'gentityaltcode_add'),
 
-    (r'^overseer/add/$', views.overseer_add, {}, 'overseer_add'),
+        (r'^gentityaltcode/delete/(?P<gentityaltcode_id>\d+)/$',
+        views.gentityaltcode_delete, {}, 'gentityaltcode_delete'),
 
-    (r'^overseer/delete/(?P<overseer_id>\d+)/$',
-     views.overseer_delete, {} , 'overseer_delete'),
+        (r'^overseer/edit/(?P<overseer_id>\d+)/$',
+        views.overseer_edit, {}, 'overseer_edit'),
 
-    (r'^instrument/edit/(?P<instrument_id>\d+)/$',
-     views.instrument_edit, {} , 'instrument_edit'),
+        (r'^overseer/add/$', views.overseer_add, {}, 'overseer_add'),
 
-    (r'^instrument/add/$', views.instrument_add, {}, 'instrument_add'),
+        (r'^overseer/delete/(?P<overseer_id>\d+)/$',
+        views.overseer_delete, {}, 'overseer_delete'),
 
-    (r'^instrument/delete/(?P<instrument_id>\d+)/$',
-     views.instrument_delete, {} , 'instrument_delete'),
+        (r'^instrument/edit/(?P<instrument_id>\d+)/$',
+        views.instrument_edit, {}, 'instrument_edit'),
 
+        (r'^instrument/add/$', views.instrument_add, {}, 'instrument_add'),
 
-    (r'^add/(?P<model_name>.+)/$',
-     views.ModelAddView.as_view(), name='model_add'),
-)
+        (r'^instrument/delete/(?P<instrument_id>\d+)/$',
+        views.instrument_delete, {}, 'instrument_delete'),
+
+        url(r'^add/(?P<model_name>.+)/$', views.ModelAddView.as_view(),
+            name='model_add'),
+    )
 
 if settings.STATIC_SERVE:
-    urlpatterns += patterns('',
+    urlpatterns += patterns(
+        '',
         (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
          {'document_root': settings.MEDIA_ROOT}),
     )
-
-
