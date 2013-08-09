96a18720319c50349c7c8e7c3b6cb354e3e13100
Antonis Christofides <antonis@wikical.com>
2013-08-09 19:19:50 +0300

    Removed custom django-registration urls
    django-registration has gone a long way since it was installed in this
    projects, and everything related to it will probably need to be
    rewritten. We begin by just removing all custom functionality.

--- enhydris/urls-base.py
+++ enhydris/urls-base.py
@@ -1,37 +1,15 @@
 from django.conf.urls.defaults import include, patterns
-from django.contrib.auth.views import password_reset, password_reset_done, \
-                                      password_change, password_change_done
 from django.contrib import admin
-from registration.views import register
-from enhydris.hcore.forms import HcoreRegistrationForm
-from enhydris.hcore.views import terms, login
+from enhydris.hcore.views import terms
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^accounts/login/$', login, {'template_name': 'registration/login.html'}),
-    (r'^accounts/register/$', register, {'form_class':
-        HcoreRegistrationForm}, 'registration_register'),
-    (r'^accounts/password/reset/$', password_reset, {'template_name':
-        'registration/password_reset.html'}, 'password_reset'),
-    (r'^accounts/password/reset/done/$', password_reset_done,
-        {'template_name': 'registration/password_reset_done.html'},
-        'password_reset_done'),
-    (r'^accounts/password/change/$', password_change, {'template_name':
-        'registration/password_change.html'}, 'password_change'),
-    (r'^accounts/password/change/done/$', password_change_done,
-        {'template_name': 'registration/password_change_done.html'},
-        'password_change_done'),
-
+urlpatterns = patterns(
+    '',
 
     (r'^accounts/', include('registration.urls')),
-
-    # terms of usage
-    (r'^terms/$', terms,{}, 'terms'),
-
-    # internationalization
+    (r'^terms/$', terms, {}, 'terms'),
     (r'^i18n/', include('django.conf.urls.i18n')),
-
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
     (r'^grappelli/', include('grappelli.urls')),
