45bdda58a54dea459ac117993f8e5ad061cf53da
Antonis Christofides <antonis@wikical.com>
2013-08-12 11:18:39 +0300

    Restored urls for password resetting

--- enhydris/urls-base.py
+++ enhydris/urls-base.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import include, patterns
 from django.contrib import admin
+from django.contrib.auth.views import password_reset, password_reset_done
 from enhydris.hcore.views import terms
 
 admin.autodiscover()
@@ -7,6 +8,11 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
 
+    (r'^accounts/password/reset/$', password_reset,
+     {'template_name': 'registration/password_reset.html'}, 'password_reset'),
+    (r'^accounts/password/reset/done/$', password_reset_done,
+     {'template_name': 'registration/password_reset_done.html'},
+     'password_reset_done'),
     (r'^accounts/', include('registration.urls')),
     (r'^terms/$', terms, {}, 'terms'),
     (r'^i18n/', include('django.conf.urls.i18n')),
