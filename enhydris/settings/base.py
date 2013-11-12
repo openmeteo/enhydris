31fd1d753c173ce566318cbe52562d9ed168f1c2
Antonis Christofides <antonis@wikical.com>
2013-11-12 15:13:59 +0200

    Better organized settings files

--- enhydris/settings/base.py
+++ enhydris/settings/base.py
@@ -1,47 +1,4 @@
-DEBUG = True
-TEMPLATE_DEBUG = False
-
 ROOT_URLCONF = 'enhydris.urls'
-
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
-
-MANAGERS = ADMINS
-
-DATABASES =  {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'openmeteo',
-        'USER': 'openmeteo',
-        'PASSWORD': 'openmeteo',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
-TIME_ZONE = 'Europe/Athens'
-
-SITE_ID = 1
-SITE_URL = "hydroscope.gr"
-
-MEDIA_ROOT = '/tmp'
-MEDIA_URL = '/site_media/'
-STATIC_ROOT = 'static/'
-STATIC_URL = '/enhydris-static/'
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'yy)g)w2jqkpyv9$w39i9$7(6wb+$h(_+x3gj#=@fzs2tmuj$#='
-
-# Options for django-registration
-ACCOUNT_ACTIVATION_DAYS = 7
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST = 'smtp.my.domain'
-DEFAULT_FROM_EMAIL = 'user@host.domain'
-SERVER_EMAIL = DEFAULT_FROM_EMAIL
-EMAIL_HOST_USER = 'automaticsender@my.domain'
-EMAIL_HOST_PASSWORD = 'mypassword'
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -102,3 +59,7 @@ TEMPLATE_DIRS = ('enhydris/templates',)
 
 AUTH_PROFILE_MODULE = 'hcore.UserProfile'
 LOGIN_REDIRECT_URL = '/'
+
+# Options for django-registration
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_OPEN = True
