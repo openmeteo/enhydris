4c4fd59e1213e94c8fb9f9707109845768269653
Antonis Christofides <antonis@wikical.com>
2014-03-17 15:50:14 +0200

    Added blank=True to some nullable fields
    The Permission model could not be manipulated in the admin because
    it didn't have blank=True, although it did have null=True.

--- enhydris/permissions/models.py
+++ enhydris/permissions/models.py
@@ -2,15 +2,16 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 from django.contrib.auth.models import User, Group
-from django.contrib import admin
+
 
 class Permission(models.Model):
     name = models.CharField(max_length=16)
-    content_type = models.ForeignKey(ContentType, related_name="row_permissions")
+    content_type = models.ForeignKey(ContentType,
+                                     related_name="row_permissions")
     object_id = models.PositiveIntegerField()
     content_object = generic.GenericForeignKey('content_type', 'object_id')
-    user = models.ForeignKey(User, null=True)
-    group = models.ForeignKey(Group, null=True)
+    user = models.ForeignKey(User, null=True, blank=True)
+    group = models.ForeignKey(Group, null=True, blank=True)
 
     class Meta:
         verbose_name = 'permission'
