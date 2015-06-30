d97ce8da0b80895b87272e71a263e4e69cc80578
Antonis Christofides <anthony@itia.ntua.gr>
2015-06-30 16:08:00 +0300

    Fix running tests with setup.py
    Fixed "python setup.py test", which had been broken after the Django
    upgrade.

--- runtests.py
+++ runtests.py
@@ -3,13 +3,14 @@
 import os
 import sys
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'test_settings'
-
+import django
 from django.test.utils import get_runner
 from django.conf import settings
 
 
 def runtests():
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'test_settings'
+    django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner(verbosity=1, interactive=True)
     failures = test_runner.run_tests(['enhydris'])
--- test_settings.py
+++ test_settings.py
@@ -23,8 +23,7 @@ MEDIA_ROOT = '/tmp'
 MEDIA_URL = '/site_media/'
 STATIC_ROOT = 'static/'
 STATIC_URL = '/enhydris-static/'
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'yy)g)w2jqkpyv9$w39i9$7(6wb+$h(_+x3gj#=@fzs2tmuj$#='
+SECRET_KEY = 'irrelevant'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.my.domain'
@@ -32,6 +31,5 @@ DEFAULT_FROM_EMAIL = 'user@host.domain'
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 EMAIL_HOST_USER = 'automaticsender@my.domain'
 EMAIL_HOST_PASSWORD = 'mypassword'
-
 
 ROOT_URLCONF = 'enhydris.urls'
