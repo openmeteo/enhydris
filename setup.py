b47526d1dd0c03b2a45dc3b5b10fa75de578846f
Antonis Christofides <anthony@itia.ntua.gr>
2015-07-09 14:02:22 +0300

    Fix package data missing from enhydris package

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+import itertools
+import os
+
 from setuptools import setup, find_packages
 
 
@@ -16,6 +19,40 @@ installation_requirements = [
     "django-bootstrap3>=5.1,<5.2",
 ]
 
+
+def get_recursive_package_data(packagedir, subdirs):
+    """
+    Return a list of files, not directories, in packagedir, recursively.
+
+    subdirs are a list of subdirectories of packagedir whose files to include.
+    The list of paths returned are relative, starting inside packagedir.
+    """
+    result = []
+    saved_cwd = os.getcwd()
+    os.chdir(packagedir)
+    try:
+        items = itertools.chain.from_iterable([os.walk(d) for d in subdirs])
+        for dirpath, dirnames, filenames in items:
+            result.extend([os.path.join(dirpath, f) for f in filenames])
+        return result
+    finally:
+        os.chdir(saved_cwd)
+
+
+packages = find_packages(exclude=['*.tests', '*.tests.*', 'tests.*', 'tests',
+                                  'enhydris.conf', 'enhydris.hcore.templates'])
+
+# Contrary to appearances, enhydris.conf isn't a package; it's a data
+# directory. The .py files it contains are files and templates to be copied
+# by the enhydris-admin command. This is why we have excluded it above. We now
+# proceed to add it as package data.
+package_data = {
+    'enhydris': ['conf/*'],
+    'enhydris.hcore': get_recursive_package_data(
+        os.path.join('enhydris', 'hcore'), ['templates', 'static']),
+}
+
+
 kwargs = {
     'name': "enhydris",
     'version': __import__('enhydris').__version__,
@@ -23,7 +60,8 @@ kwargs = {
     'description': "Web application for meteorological data storage",
     'author': "Antonis Christofides",
     'author_email': "anthony@itia.ntua.gr",
-    'packages': find_packages(),
+    'packages': packages,
+    'package_data': package_data,
     'scripts': ['enhydris/bin/enhydris-admin'],
     'install_requires': installation_requirements,
     'test_suite': 'runtests.runtests',
