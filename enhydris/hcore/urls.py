4e7421797f8d93ca2c56c3313f0e06f9998b6b6d
Stavros Anastasiadis <anastasiadis.st00@gmail.com>
2015-09-07 14:26:25 +0300

    Add registration Bootstraped templates
    In order to overide django default registration templates
    modifications in settings.base are also made.

--- enhydris/hcore/urls.py
+++ enhydris/hcore/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 from django.views.generic.base import RedirectView
+from django.contrib.auth import views as auth_views
 
 from enhydris.hcore import views
 
@@ -91,4 +92,18 @@ urlpatterns = patterns(
 
     url(r'^add/(?P<model_name>.+)/$', views.ModelAddView.as_view(),
         name='model_add'),
+
+    #   http://stackoverflow.com/questions/19985103/
+    url(r'^password/change/$', auth_views.password_change,
+        name='password_change'),
+    url(r'^password/change/done/$', auth_views.password_change_done,
+        name='password_change_done'),
+    url(r'^password/reset/$', auth_views.password_reset,
+        name='password_reset'),
+    url(r'^accounts/password/reset/done/$', auth_views.password_reset_done,
+        name='password_reset_done'),
+    url(r'^password/reset/complete/$', auth_views.password_reset_complete,
+        name='password_reset_complete'),
+    url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
 )
