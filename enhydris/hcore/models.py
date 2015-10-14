20ad815eb9f1b90e36c1af49c8b18d005ebade31
Antonis Christofides <anthony@itia.ntua.gr>
2015-10-14 10:35:13 +0300

    Explain timestamp rounding and offset to the user
    Added some text on the time series form to explain what is meant by
    timestamp rounding and timestamp offset.

--- enhydris/hcore/models.py
+++ enhydris/hcore/models.py
@@ -496,12 +496,41 @@ class Timeseries(models.Model):
     instrument = models.ForeignKey(Instrument, null=True, blank=True)
     time_step = models.ForeignKey(TimeStep, null=True, blank=True)
     interval_type = models.ForeignKey(IntervalType, null=True, blank=True)
-    timestamp_rounding_minutes = models.PositiveIntegerField(null=True,
-                                                             blank=True)
-    timestamp_rounding_months = models.PositiveSmallIntegerField(null=True,
-                                                                 blank=True)
-    timestamp_offset_minutes = models.IntegerField(null=True, blank=True)
-    timestamp_offset_months = models.SmallIntegerField(null=True, blank=True)
+    timestamp_rounding_minutes = models.PositiveIntegerField(
+        null=True, blank=True, help_text=_(
+            "For an hourly time series whose timestamps end in :00, set this "
+            "to zero; if they end in :12, set it to 12. For a ten-minute time "
+            "series with timestamps ending in :12, :22, :32, etc., set it to "
+            "2.  For daily ending at 08:00, set it to 480. Leave empty if "
+            "timestamps are irregular."
+        )
+    )
+    timestamp_rounding_months = models.PositiveSmallIntegerField(
+        null=True, blank=True, help_text=_(
+            "Set this to zero, except for annual time series, indicating the "
+            "difference from January; for example, set it to 9 if the "
+            "timestamps use a hydrological year starting in October. Leave "
+            "empty if timestamps are irregular."
+        )
+    )
+    timestamp_offset_minutes = models.IntegerField(
+        null=True, blank=True, help_text=_(
+            "If unsure, set this to zero. It indicates the difference of "
+            "what is shown from what is meant. For example, if for an hourly "
+            "time series it is -5, then 2015-10-14 11:00 means the interval "
+            "from 2015-10-14 09:55 to 2015-10-14 10:55. -1440 is common for "
+            "daily time series."
+        )
+    )
+    timestamp_offset_months = models.SmallIntegerField(
+        null=True, blank=True, help_text=_(
+            "If unsure, set this to 1 for monthly, 12 for annual, and zero "
+            "otherwise.  For a monthly time series, an offset of -475 "
+            "minutes and 1 month means that 2003-11-01 00:00 (normally "
+            "shown as 2003-11) denotes the interval 2003-10-31 18:05 to "
+            "2003-11-30 18:05."
+        )
+    )
     class Meta:
         verbose_name = "Time Series"
         verbose_name_plural = "Time Series"
