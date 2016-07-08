1670dd840048bee7d66267484c0ae59d6fd91738
Antonis Christofides <anthony@itia.ntua.gr>
2016-07-08 15:41:16 +0300

    Fix tests that failed depending on datadir setting

--- enhydris/hcore/tests/test_views.py
+++ enhydris/hcore/tests/test_views.py
@@ -29,6 +29,7 @@ from enhydris.hcore.models import (
     EventType, FileType, GentityEvent, GentityFile, Instrument, InstrumentType,
     Organization, Station, StationType, Timeseries, TimeZone,
     UnitOfMeasurement, UserProfile, Variable)
+from enhydris.hcore.tests.test_models import RandomEnhydrisTimeseriesDataDir
 from enhydris.hcore.views import ALLOWED_TO_EDIT, StationListBaseView
 
 try:
@@ -306,25 +307,6 @@ class RandomMediaRoot(override_settings):
 
     def disable(self):
         super(RandomMediaRoot, self).disable()
-        shutil.rmtree(self.tmpdir)
-
-
-class RandomEnhydrisTimeseriesDataDir(override_settings):
-    """
-    Override ENHYDRIS_TIMESERIES_DATA_DIR to a temporary directory.
-
-    Specifying "@RandomEnhydrisTimeseriesDataDir()" as a decorator is the same
-    as "@override_settings(ENHYDRIS_TIMESERIES_DATA_DIR=tempfile.mkdtemp())",
-    except that in the end it removes the temporary directory.
-    """
-
-    def __init__(self):
-        self.tmpdir = tempfile.mkdtemp()
-        super(RandomEnhydrisTimeseriesDataDir, self).__init__(
-            ENHYDRIS_TIMESERIES_DATA_DIR=self.tmpdir)
-
-    def disable(self):
-        super(RandomEnhydrisTimeseriesDataDir, self).disable()
         shutil.rmtree(self.tmpdir)
 
 
