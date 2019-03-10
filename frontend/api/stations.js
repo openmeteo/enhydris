64da08fc04b31bcbd3ba9f376a0c7a8079a435d1
Stavros Anastasiadis <anastasiadis.st00@gmail.com>
2019-03-10 08:12:07 +0200

    fix(stations): Fix handling of API pagination in the frontend (fixes #116)

--- frontend/api/stations.js
+++
@@ -1,7 +0,0 @@
-import session from "./session";
-
-export default {
-  list: () => session.get("/stations/"),
-  types: () => session.get("/stationtypes/"),
-  divisions: () => session.get("/politicaldivisions/")
-};
--- frontend/utils/index.js
+++
@@ -1,7 +0,0 @@
-const utils = {
-  getApiRoot: () => process.env.API_ROOT,
-  getAppMode: () => process.env.NODE_ENV,
-  getAppTitle: () => process.env.APP_TITLE
-};
-
-export default utils;
