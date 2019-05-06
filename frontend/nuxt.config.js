1c18a14beadf9a265baa5e1c4db941f9ac59ee3c
Stavros Anastasiadis <anastasiadis.st00@gmail.com>
2019-05-06 08:49:46 +0300

    Remove og:description (fixes #166)

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -11,12 +11,7 @@ module.exports = {
     titleTemplate: `%s — Enhydris`,
     meta: [
       { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      {
-        hid: "description",
-        name: "description",
-        content: "Enhydris Client App"
-      }
+      { name: "viewport", content: "width=device-width, initial-scale=1" }
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
