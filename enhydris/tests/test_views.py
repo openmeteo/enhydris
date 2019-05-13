9ad74f4e919407106248dc311c3bffe180c63b20
Antonis Christofides <antonis@antonischristofides.com>
2019-05-13 18:31:47 +0300

    Add unit tests for StationList

--- enhydris/tests/test_views.py
+++ enhydris/tests/test_views.py
@@ -1,13 +1,96 @@
 from unittest import skipUnless
 
 from django.conf import settings
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
 from django.contrib.gis.geos import Point
+from django.test import TestCase, override_settings
 
+from bs4 import BeautifulSoup
 from django_selenium_clean import PageElement, SeleniumTestCase
 from model_mommy import mommy
 from selenium.webdriver.common.by import By
 
 from enhydris.models import Station
+
+
+class StationListTestCase(TestCase):
+    def setUp(self):
+        mommy.make(
+            User,
+            username="admin",
+            password=make_password("topsecret"),
+            is_active=True,
+            is_superuser=True,
+            is_staff=True,
+        )
+        mommy.make(
+            Station,
+            name="Komboti",
+            point=Point(x=21.06071, y=39.09518, srid=4326),
+            srid=4326,
+        )
+        mommy.make(
+            Station,
+            name="Agios Athanasios",
+            point=Point(x=21.60121, y=39.22440, srid=4326),
+            srid=4326,
+        )
+        mommy.make(
+            Station,
+            name="Tharbad",
+            point=Point(x=-176.48368, y=0.19377, srid=4326),
+            srid=4326,
+        )
+        mommy.make(
+            Station,
+            name="SRID Point, NoSRID Station",
+            point=Point(x=-176.48368, y=0.19377, srid=4326),
+            srid=None,
+        )
+        mommy.make(
+            Station,
+            name="NoSRID Point, SRID Station",
+            point=Point(x=-176.48368, y=0.19377, srid=None),
+            srid=4326,
+        )
+        mommy.make(
+            Station,
+            name="NoSRID Point, NoSRID Station",
+            point=Point(x=-176.48368, y=0.19377, srid=None),
+            srid=None,
+        )
+
+    def test_station_list(self):
+        response = self.client.get("/")
+        self.assertContains(
+            response, '<a href="?sort=-name&amp;sort=name">Name&nbsp;↓</a>', html=True
+        )
+
+    @override_settings(ENHYDRIS_STATIONS_PER_PAGE=3)
+    def test_two_pages(self):
+        response = self.client.get("/")
+        self.assertContains(response, "<a href='?page=2'>2</a>", html=True)
+        self.assertNotContains(response, "<a href='?page=3'>3</a>", html=True)
+
+    @override_settings(ENHYDRIS_STATIONS_PER_PAGE=2)
+    def test_next_page_url(self):
+        response = self.client.get("/")
+        soup = BeautifulSoup(response.content, "html.parser")
+        next_page_url = soup.find("a", id="next-page").get("href")
+        self.assertEqual(next_page_url, "?page=2")
+
+    @override_settings(ENHYDRIS_STATIONS_PER_PAGE=2)
+    def test_previous_page_url(self):
+        response = self.client.get("/?page=2")
+        soup = BeautifulSoup(response.content, "html.parser")
+        next_page_url = soup.find("a", id="previous-page").get("href")
+        self.assertEqual(next_page_url, "?page=1")
+
+    @override_settings(ENHYDRIS_STATIONS_PER_PAGE=100)
+    def test_one_page(self):
+        response = self.client.get("/")
+        self.assertNotContains(response, "<a href='?page=2'>2</a>", html=True)
 
 
 @skipUnless(getattr(settings, "SELENIUM_WEBDRIVERS", False), "Selenium is unconfigured")
