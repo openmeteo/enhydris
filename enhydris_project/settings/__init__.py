a8259c3f82ea47550dc22ec318143fb8ef3926c8
Antonis Christofides <antonis@antonischristofides.com>
2019-05-20 13:01:27 +0300

    Use session authentication in the API (fixes #192)

--- enhydris_project/settings/__init__.py
+++ enhydris_project/settings/__init__.py
@@ -94,6 +94,7 @@ AUTHENTICATION_BACKENDS = (
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
