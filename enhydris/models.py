openmeteo/enhydris

commit d003d7d95d5f15314918153d55dd55305d69ce98

Simplify co-ordinate fields (fixes #132)
This commit abolishes mostly unused fields "approximate" and "asrid" and
renames "srid" to "original_srid".

enhydris/models.py
@@ -132,16 +132,14 @@ class Gentity(models.Model):
 
 
 class Gpoint(Gentity):
-    srid = models.IntegerField(null=True, blank=True)
-    approximate = models.BooleanField(default=False)
+    original_srid = models.IntegerField(null=True, blank=True)
     altitude = models.FloatField(null=True, blank=True)
-    asrid = models.IntegerField(null=True, blank=True)
     f_dependencies = ["Gentity"]
     point = models.PointField()
 
     def original_abscissa(self):
-        if self.point and self.srid:
-            (x, y) = self.point.transform(self.srid, clone=True)
+        if self.point and self.original_srid:
+            (x, y) = self.point.transform(self.original_srid, clone=True)
             return round(x, 2) if abs(x) > 180 and abs(y) > 90 else x
         elif self.point:
             return self.point.x
@@ -149,8 +147,8 @@ class Gpoint(Gentity):
             return None
 
     def original_ordinate(self):
-        if self.point and self.srid:
-            (x, y) = self.point.transform(self.srid, clone=True)
+        if self.point and self.original_srid:
+            (x, y) = self.point.transform(self.original_srid, clone=True)
             return round(y, 2) if abs(x) > 180 and abs(y) > 90 else y
         elif self.point:
             return self.point.y
@@ -569,9 +567,8 @@ class Timeseries(models.Model):
             location = {
                 "abscissa": self.gentity.gpoint.point[0],
                 "ordinate": self.gentity.gpoint.point[1],
-                "srid": self.gentity.gpoint.srid,
+                "original_srid": self.gentity.gpoint.original_srid,
                 "altitude": self.gentity.gpoint.altitude,
-                "asrid": self.gentity.gpoint.asrid,
             }
         except TypeError:
             # TypeError occurs when self.gentity.gpoint.point is None,

enhydris/tests/test_models.py
@@ -352,7 +352,7 @@ class StationOriginalCoordinatesTestCase(TestCase):
             models.Station,
             name="Komboti",
             point=Point(x=21.06071, y=39.09518, srid=4326),
-            srid=2100,
+            original_srid=2100,
         )
         self.station = models.Station.objects.get(name="Komboti")
 
@@ -369,7 +369,7 @@ class StationOriginalCoordinatesWithNullSridTestCase(TestCase):
             models.Station,
             name="Komboti",
             point=Point(x=21.06071, y=39.09518, srid=4326),
-            srid=None,
+            original_srid=None,
         )
         self.station = models.Station.objects.get(name="Komboti")
 
@@ -628,10 +628,9 @@ class TimeseriesGetDataTestCase(TestCase):
         station = mommy.make(
             models.Station,
             name="Celduin",
-            srid=4326,
+            original_srid=4326,
             point=Point(x=21.06071, y=39.09518, srid=4326),
             altitude=219,
-            asrid=None,
         )
         cls.timeseries = mommy.make(
             models.Timeseries,
@@ -663,14 +662,11 @@ class TimeseriesGetDataTestCase(TestCase):
     def test_ordinate(self):
         self.assertAlmostEqual(self.data.location["ordinate"], 39.09518)
 
-    def test_srid(self):
-        self.assertAlmostEqual(self.data.location["srid"], 4326)
+    def test_original_srid(self):
+        self.assertAlmostEqual(self.data.location["original_srid"], 4326)
 
     def test_altitude(self):
         self.assertAlmostEqual(self.data.location["altitude"], 219)
-
-    def test_asrid(self):
-        self.assertIsNone(self.data.location["asrid"])
 
     def test_time_step(self):
         self.assertEqual(self.data.time_step, "60,0")

enhydris/admin/station.py
@@ -45,21 +45,13 @@ class StationAdminForm(forms.ModelForm):
         label=_("Co-ordinates"),
         help_text=_("Longitude and latitude in decimal degrees"),
     )
-    srid = forms.IntegerField(
+    original_srid = forms.IntegerField(
         label=_("Original SRID"),
         required=False,
         help_text=_(
             "Set this to 4326 if you have no idea what we're talking about. "
             "If the latitude and longitude has been converted from another co-ordinate "
             "system, enter the SRID of the original co-ordinate system."
-        ),
-    )
-    asrid = forms.IntegerField(
-        label=_("Altitude SRID"),
-        required=False,
-        help_text=_(
-            "Leave this empty if unsure. Set it to the SRID of the reference system "
-            "used for the altitude. Empty means mean sea level."
         ),
     )
 
@@ -328,8 +320,8 @@ class StationAdmin(ObjectPermissionsModelAdmin):
                         "political_division",
                         "water_division",
                         "water_basin",
-                        ("point", "srid", "approximate"),
-                        ("altitude", "asrid"),
+                        ("point", "original_srid"),
+                        ("altitude"),
                     ),
                     "classes": ("collapse",),
                 },

enhydris/api/csv.py
@@ -13,7 +13,6 @@ _station_list_csv_headers = [
     "Abscissa",
     "Ordinate",
     "SRID",
-    "Approximate",
     "Altitude",
     "SRID",
     "Water basin",
@@ -36,10 +35,8 @@ def _station_csv(s):
         s.end_date,
         s.original_abscissa(),
         s.original_ordinate(),
-        s.srid,
-        s.approximate,
+        s.original_srid,
         s.altitude,
-        s.asrid,
         s.water_basin.name if s.water_basin else "",
         s.water_division.name if s.water_division else "",
         s.political_division.name if s.political_division else "",

enhydris/api/tests/test_views/test_bounding_box.py
@@ -10,10 +10,14 @@ from enhydris import models
 class SimpleBoundingBoxTestCase(APITestCase):
     def setUp(self):
         mommy.make(
-            models.Station, point=Point(x=21.06071, y=39.09518, srid=4326), srid=4326
+            models.Station,
+            point=Point(x=21.06071, y=39.09518, srid=4326),
+            original_srid=4326,
         )
         mommy.make(
-            models.Station, point=Point(x=21.60121, y=39.22440, srid=4326), srid=4326
+            models.Station,
+            point=Point(x=21.60121, y=39.22440, srid=4326),
+            original_srid=4326,
         )
         response = self.client.get("/api/stations/")
         self.bounding_box = response.json()["bounding_box"]
@@ -54,10 +58,14 @@ class DefaultBoundingBoxTestCase(APITestCase):
 class TooSmallBoundingBoxTestCase(APITestCase):
     def setUp(self):
         mommy.make(
-            models.Station, point=Point(x=21.06071, y=39.09518, srid=4326), srid=4326
+            models.Station,
+            point=Point(x=21.06071, y=39.09518, srid=4326),
+            original_srid=4326,
         )
         mommy.make(
-            models.Station, point=Point(x=21.60121, y=39.22440, srid=4326), srid=4326
+            models.Station,
+            point=Point(x=21.60121, y=39.22440, srid=4326),
+            original_srid=4326,
         )
         response = self.client.get("/api/stations/")
         self.bounding_box = response.json()["bounding_box"]

enhydris/api/tests/test_views/test_station.py
@@ -157,37 +157,37 @@ class StationCsvTestCase(APITestCase):
             models.Station,
             name="Komboti",
             point=Point(x=21.06071, y=39.09518, srid=4326),
-            srid=4326,
+            original_srid=4326,
         )
         mommy.make(
             models.Station,
             name="Agios Athanasios",
             point=Point(x=21.60121, y=39.22440, srid=4326),
-            srid=4326,
+            original_srid=4326,
         )
         mommy.make(
             models.Station,
             name="Tharbad",
             point=Point(x=-176.48368, y=0.19377, srid=4326),
-            srid=4326,
+            original_srid=4326,
         )
         mommy.make(
             models.Station,
             name="SRID Point, NoSRID Station",
             point=Point(x=-176.48368, y=0.19377, srid=4326),
-            srid=None,
+            original_srid=None,
         )
         mommy.make(
             models.Station,
             name="NoSRID Point, SRID Station",
             point=Point(x=-176.48368, y=0.19377, srid=None),
-            srid=4326,
+            original_srid=4326,
         )
         mommy.make(
             models.Station,
             name="NoSRID Point, NoSRID Station",
             point=Point(x=-176.48368, y=0.19377, srid=None),
-            srid=None,
+            original_srid=None,
         )
 
     def test_station_csv(self):
@@ -198,7 +198,7 @@ class StationCsvTestCase(APITestCase):
                 stations_csv = f.open("stations.csv").read().decode()
                 self.assertIn(",Agios Athanasios,", stations_csv)
 
-    def test_station_with_no_srid_is_included(self):
+    def test_station_with_no_original_srid_is_included(self):
         response = self.client.get("/api/stations/csv/")
         with tempfile.TemporaryFile() as t:
             t.write(response.content)
@@ -206,7 +206,7 @@ class StationCsvTestCase(APITestCase):
                 stations_csv = f.open("stations.csv").read().decode()
                 self.assertIn("SRID Point, NoSRID Station", stations_csv)
 
-    def test_station_with_point_with_no_srid_is_included(self):
+    def test_station_with_point_with_no_original_srid_is_included(self):
         response = self.client.get("/api/stations/csv/")
         with tempfile.TemporaryFile() as t:
             t.write(response.content)

enhydris/locale/el/LC_MESSAGES/django.po
@@ -471,10 +471,6 @@ msgstr "Τεταγμένη"
 msgid "SRID"
 msgstr "SRID"
 
-#: templates/station_detail.html:182
-msgid "Approximate"
-msgstr "Προσέγγιση"
-
 #: templates/station_detail.html:186
 msgid "Altitude"
 msgstr "Υψόμετρο"

enhydris/locale/locale/en/LC_MESSAGES/django.po
@@ -287,10 +287,6 @@ msgstr ""
 msgid "SRID"
 msgstr ""
 
-#: templates/enhydris/station_detail.html:66
-msgid "Approximate"
-msgstr ""
-
 #: templates/enhydris/station_detail.html:70
 msgid "Altitude"
 msgstr ""

commit d583c96addb58b3d17f257c1008a2a5075c0b9c3

Fix Location attribute of hts files (fixes #202)
This fixes a regression caused by d003d7d.

enhydris/models.py
@@ -562,16 +562,14 @@ class Timeseries(models.Model):
             )
 
     def _set_extra_timeseries_properties(self, ahtimeseries):
-        try:
+        if self.gentity.gpoint.point:
             location = {
-                "abscissa": self.gentity.gpoint.point[0],
-                "ordinate": self.gentity.gpoint.point[1],
-                "original_srid": self.gentity.gpoint.original_srid,
+                "abscissa": self.gentity.gpoint.original_abscissa(),
+                "ordinate": self.gentity.gpoint.original_ordinate(),
+                "srid": self.gentity.gpoint.original_srid,
                 "altitude": self.gentity.gpoint.altitude,
             }
-        except TypeError:
-            # TypeError occurs when self.gentity.gpoint.point is None,
-            # meaning the co-ordinates aren't registered.
+        else:
             location = None
         ahtimeseries.time_step = "{},{}".format(
             self.time_step.length_minutes if self.time_step else 0,

enhydris/tests/test_models.py
@@ -628,7 +628,7 @@ class TimeseriesGetDataTestCase(TestCase):
         station = mommy.make(
             models.Station,
             name="Celduin",
-            original_srid=4326,
+            original_srid=2100,
             point=Point(x=21.06071, y=39.09518, srid=4326),
             altitude=219,
         )
@@ -657,13 +657,13 @@ class TimeseriesGetDataTestCase(TestCase):
         cls.data = cls.timeseries.get_data()
 
     def test_abscissa(self):
-        self.assertAlmostEqual(self.data.location["abscissa"], 21.06071)
+        self.assertAlmostEqual(self.data.location["abscissa"], 245648.96, places=2)
 
     def test_ordinate(self):
-        self.assertAlmostEqual(self.data.location["ordinate"], 39.09518)
+        self.assertAlmostEqual(self.data.location["ordinate"], 4331165.20, places=2)
 
-    def test_original_srid(self):
-        self.assertAlmostEqual(self.data.location["original_srid"], 4326)
+    def test_srid(self):
+        self.assertAlmostEqual(self.data.location["srid"], 2100)
 
     def test_altitude(self):
         self.assertAlmostEqual(self.data.location["altitude"], 219)