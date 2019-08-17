99bc761710056cf828853e7b135edba6146a7a95
Antonis Christofides <antonis@antonischristofides.com>
2019-08-17 18:52:05 +0300

    Move LatLonWidget to django-geowidgets (fixes #225)

--- enhydris/admin/station.py
+++ enhydris/admin/station.py
@@ -6,38 +6,11 @@ from django.contrib import admin
 from django.db.models import Q, TextField
 from django.utils.translation import ugettext_lazy as _
 
+from geowidgets import LatLonField
 from htimeseries import HTimeseries
 from rules.contrib.admin import ObjectPermissionsModelAdmin
 
 from enhydris import models
-
-
-class LatLonWidget(forms.MultiWidget):
-    """A simple widget showing latitude and longitude."""
-
-    def __init__(self, *args, **kwargs):
-        widgets = forms.TextInput(), forms.TextInput()
-        super().__init__(widgets, *args, **kwargs)
-
-    def decompress(self, value):
-        if value:
-            return [round(x, 6) for x in value.coords]
-        return (None, None)
-
-
-class LatLonField(forms.MultiValueField):
-    widget = LatLonWidget
-    require_all_fields = True
-
-    def __init__(self, *args, **kwargs):
-        fields = (
-            forms.FloatField(min_value=-180, max_value=180),
-            forms.FloatField(min_value=-90, max_value=90),
-        )
-        super().__init__(fields, *args, **kwargs)
-
-    def compress(self, data_list):
-        return "SRID=4326;POINT({} {})".format(*data_list)
 
 
 class StationAdminForm(forms.ModelForm):
--- enhydris/tests/admin/test_station.py
+++ enhydris/tests/admin/test_station.py
@@ -3,34 +3,13 @@ from io import StringIO
 from locale import LC_CTYPE, getlocale, setlocale
 
 from django.contrib.auth.models import Permission, User
-from django.contrib.gis.geos import Point
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase, override_settings
 
 from model_mommy import mommy
 
-from enhydris.admin.station import LatLonField, LatLonWidget, TimeseriesInlineAdminForm
+from enhydris.admin.station import TimeseriesInlineAdminForm
 from enhydris.models import Organization, Station, StationType, Timeseries, TimeStep
-
-
-class LatLonWidgetTestCase(TestCase):
-    def test_decompress_value(self):
-        result = LatLonWidget().decompress(Point(12.34567891234567, -23.456789123456))
-        self.assertAlmostEqual(result[0], 12.345679, places=13)
-        self.assertAlmostEqual(result[1], -23.456789, places=13)
-
-    def test_decompress_none(self):
-        result = LatLonWidget().decompress(None)
-        self.assertIsNone(result[0])
-        self.assertIsNone(result[1])
-
-
-class LatLonFieldTestCase(TestCase):
-    def test_compress(self):
-        self.assertEqual(
-            LatLonField().compress([12.345678, -23.456789]),
-            "SRID=4326;POINT(12.345678 -23.456789)",
-        )
 
 
 class StationPermsTestCaseBase(TestCase):
