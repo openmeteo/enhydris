f68cb1189e9dfb910e35923ec980c9ce4b0d779d
Eyob Tariku <eyobtariku@gmail.com>
2020-11-18 20:12:06 +0300

    Add automated JS linting & testing infrastructure (fixes #367)

--- enhydris/static/js/chart.js
+++ enhydris/static/js/chart.js
@@ -1,232 +1,231 @@
-$(function() {
-	const config = {
-		blue: "#008FFB",
-		gray: "#546E7A",
-	};
+$(function () {
+  const chartConfigs = {
+    blue: '#008FFB',
+    gray: '#546E7A',
+    miniChart: null,
+    mainChart: null,
+  };
 
-	/* When attempting to select section of the mini ("small") chart,
-	the `selection` event of the chart is executed multiple times for each
-	mouse drags. Since the `refetchChart` function is part of the `selection`
-	event, this causes multiple API calls to the chart endpoint before the user
-	has completed the selection. This is not efficient. Thus, by using the
-	`debounce` function, we can create another function that delay the execution
-	of the `refetchChart` function by a certain amount of milliseconds, in order
-	to give the user enough time to complete the selection. 
-	*/
-	const debounceRefetchChart = debounce(refetchChart, 200);
+  function mapData(data) {
+    return data.map((i) => [i.timestamp * 1000, Number(i.value) || 0]);
+  }
 
-	const mainChartOption = {
-		series: [{
-			data: []
-		}],
-		noData: {
-			text: enhydris.strLoading
-		},
-		chart: {
-			id: "mainChart",
-			type: "line",
-			height: 230,
-			zoom: {
-				type: "x",
-				enabled: true,
-				autoScaleYaxis: true,
-			},
-			toolbar: {
-				autoSelected: "zoom",
-				show: true,
-				tools: {
-					download: true,
-					zoomin: true,
-					zoomout: true,
-					pan: false,
-					reset: false
-				},
-			},
-			events: {
-				zoomed: function (chartContext, { xaxis }) {
-					refetchChart(xaxis);
-					miniChart.updateOptions({
-						chart: {
-							selection: {
-								xaxis: xaxis
-							}       
-						}
-					});
-				},
-			},
-		},
-		colors: [config.gray],
-		stroke: {
-			width: 2
-		},
-		dataLabels: {
-			enabled: false
-		},
-		fill: {
-			opacity: 1
-		},
-		markers: {
-			size: 0
-		},
-		xaxis: {
-			type: "datetime",
-			tooltip: {
-				enabled: false
-			}
-		},
-		tooltip: {
-			enabled: true,
-			x: {
-				show: true,
-				format: "MMM dd, yyyy"
-			}
-		}
-	};
+  function formatDate(timestamp) {
+    return new Date(timestamp).toISOString().substring(0, 16);
+  }
 
-	const mainChart = new ApexCharts(
-		document.querySelector("#chart-main"),
-		mainChartOption
-	);
-	mainChart.render();
+  function debounce(func, wait, immediate = false) {
+    /* Returns a modified callable that waits a certain amount of time
+    (i.e. the value of `wait` parameter) before running the function. If the
+    function attempts to execute within the wait time, the call is blocked.
 
-	const miniChartOptions = {
-		series: [{
-				data: []
-		}],
-		chart: {
-			id: "miniChart",
-			height: 100,
-			type: "area",
-			brush: {
-				target: "mainChart",
-				enabled: true
-			},
-			selection: {
-				enabled: true
-			},
-			events: {
-				selection: function (chartContext, { xaxis, yaxis }) {
-					debounceRefetchChart(xaxis);
-				}
-			},
-		},
-		colors: [config.blue],
-		fill: {
-			type: "gradient",
-			gradient: {
-				opacityFrom: 0.91,
-				opacityTo: 0.1
-			},
-		},
-		xaxis: {
-			type: "datetime",
-			tooltip: {
-				enabled: false
-			},
-			labels: {
-				show: true,
-				datetimeFormatter: {
-					year: "yyyy",
-					month: "MMM 'yy",
-					day: "dd MMM",
-					hour: "HH:mm"
-				},
-			},
-		},
-		yaxis: {
-			tickAmount: 2
-		},
-	};
+    To read more about it:
+    https://educative.io/edpresso/how-to-use-the-debounce-function-in-javascript
+    */
+    let timeout;
 
-	const miniChart = new ApexCharts(
-		document.querySelector("#chart-mini"),
-		miniChartOptions
-	);
-	miniChart.render();
+    return (...args) => {
+      const later = () => {
+        timeout = null;
+        if (!immediate) func.apply(this, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(this, args);
+    };
+  }
 
-	fetch(enhydris.chartApiUrl)
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (data) {
-			if (data && data[0]) {
-				const mappedData = mapData(data);
+  function renderError() {
+    document.querySelector('#data_holder').innerHTML = `<p>${enhydris.strNoData}</p>`;
+  }
 
-				mainChart.updateSeries([
-					{
-						data: mappedData,
-					},
-				]);
-				miniChart.updateSeries([
-					{
-						data: mappedData,
-					},
-				]);
-			} else {
-				renderError();
-			}
-		})
-		.catch(function () {
-			renderError();
-		});
+  function refetchChart({ min, max }) {
+    /* Make API call to fetch & populate chart data between two selected
+    data points (i.e. start datetime and end datetime).
+    */
+    const startDate = formatDate(min);
+    const endDate = formatDate(max);
 
-	function mapData(data) {
-		return data.map((i) => [i.timestamp * 1000, Number(i.value) || 0]);
-	}
+    chartConfigs.mainChart.updateSeries([
+      {
+        data: [],
+      },
+    ]);
+    fetch(`${enhydris.chartApiUrl}?start_date=${startDate}&end_date=${endDate}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data && data[0]) {
+          chartConfigs.mainChart.updateSeries([
+            {
+              data: mapData(data),
+            },
+          ]);
+        }
+      });
+  }
 
-	function formatDate(timestamp) {
-		return new Date(timestamp).toISOString().substring(0, 16);
-	}
+  /* When attempting to select section of the mini ("small") chart,
+  the `selection` event of the chart is executed multiple times for each
+  mouse drags. Since the `refetchChart` function is part of the `selection`
+  event, this causes multiple API calls to the chart endpoint before the user
+  has completed the selection. This is not efficient. Thus, by using the
+  `debounce` function, we can create another function that delay the execution
+  of the `refetchChart` function by a certain amount of milliseconds, in order
+  to give the user enough time to complete the selection.
+  */
+  const debounceRefetchChart = debounce(refetchChart, 200);
 
-	function debounce(func, wait, immediate=false) {
-		/* Returns a modified callable that waits a certain amount of time
-		(i.e. the value of `wait` parameter) before running the function. If the
-		function attempts to execute within the wait time, the call is blocked.
-		
-		To read more about it:
-		https://educative.io/edpresso/how-to-use-the-debounce-function-in-javascript
-		*/
-		let timeout;
+  const miniChartOptions = {
+    series: [{
+      data: [],
+    }],
+    chart: {
+      id: 'miniChart',
+      height: 100,
+      type: 'area',
+      brush: {
+        target: 'mainChart',
+        enabled: true,
+      },
+      selection: {
+        enabled: true,
+      },
+      events: {
+        selection(chartContext, { xaxis }) {
+          debounceRefetchChart(xaxis);
+        },
+      },
+    },
+    colors: [chartConfigs.blue],
+    fill: {
+      type: 'gradient',
+      gradient: {
+        opacityFrom: 0.91,
+        opacityTo: 0.1,
+      },
+    },
+    xaxis: {
+      type: 'datetime',
+      tooltip: {
+        enabled: false,
+      },
+      labels: {
+        show: true,
+        datetimeFormatter: {
+          year: 'yyyy',
+          month: "MMM 'yy",
+          day: 'dd MMM',
+          hour: 'HH:mm',
+        },
+      },
+    },
+    yaxis: {
+      tickAmount: 2,
+    },
+  };
 
-		return (...args) => {
-			const later = () => {
-				timeout = null;
-				if (!immediate) func.apply(this, args)
-			}
-			const callNow = immediate && !timeout;
-			clearTimeout(timeout);
-			timeout = setTimeout(later, wait);
-			if (callNow) func.apply(this, args);
-		}
-	}
-	
-	function refetchChart({ min, max }) {
-		/* Make API call to fetch & populate chart data between two selected
-		data points (i.e. start datetime and end datetime).
-		*/
-		const startDate = formatDate(min);
-		const endDate = formatDate(max);
+  chartConfigs.miniChart = new ApexCharts(
+    document.querySelector('#chart-mini'),
+    miniChartOptions,
+  );
+  chartConfigs.miniChart.render();
 
-		mainChart.updateSeries([
-			{
-				data: [],
-			},
-		]);
-		fetch(`${enhydris.chartApiUrl}?start_date=${startDate}&end_date=${endDate}`)
-			.then(response => response.json())
-			.then(data => {
-				if (data && data[0]) {
-					mainChart.updateSeries([
-						{
-							data: mapData(data)
-						},
-					]);
-				}
-			});
-	}
+  const mainChartOption = {
+    series: [{
+      data: [],
+    }],
+    noData: {
+      text: enhydris.strLoading,
+    },
+    chart: {
+      id: 'mainChart',
+      type: 'line',
+      height: 230,
+      zoom: {
+        type: 'x',
+        enabled: true,
+        autoScaleYaxis: true,
+      },
+      toolbar: {
+        autoSelected: 'zoom',
+        show: true,
+        tools: {
+          download: true,
+          zoomin: true,
+          zoomout: true,
+          pan: false,
+          reset: false,
+        },
+      },
+      events: {
+        zoomed(chartContext, { xaxis }) {
+          refetchChart(xaxis);
+          chartConfigs.miniChart.updateOptions({
+            chart: {
+              selection: {
+                xaxis,
+              },
+            },
+          });
+        },
+      },
+    },
+    colors: [chartConfigs.gray],
+    stroke: {
+      width: 2,
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    fill: {
+      opacity: 1,
+    },
+    markers: {
+      size: 0,
+    },
+    xaxis: {
+      type: 'datetime',
+      tooltip: {
+        enabled: false,
+      },
+    },
+    tooltip: {
+      enabled: true,
+      x: {
+        show: true,
+        format: 'MMM dd, yyyy',
+      },
+    },
+  };
 
-	function renderError () {
-		document.querySelector("#data_holder").innerHTML =
-			`<p>${enhydris.strNoData}</p>`;
-	};
+  chartConfigs.mainChart = new ApexCharts(
+    document.querySelector('#chart-main'),
+    mainChartOption,
+  );
+  chartConfigs.mainChart.render();
+
+  fetch(enhydris.chartApiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data && data[0]) {
+        const mappedData = mapData(data);
+
+        chartConfigs.mainChart.updateSeries([
+          {
+            data: mappedData,
+          },
+        ]);
+        chartConfigs.miniChart.updateSeries([
+          {
+            data: mappedData,
+          },
+        ]);
+      } else {
+        renderError();
+      }
+    })
+    .catch(() => {
+      renderError();
+    });
 });
--- enhydris/static/js/enhydris.js
+++ enhydris/static/js/enhydris.js
@@ -1,74 +1,69 @@
 enhydris.mapModule = (function namespace() {
-    'use strict';
-    var map, stationsLayer;
+  let map;
+  let stationsLayer;
 
-    var init = function () {
-        _setupMap();
-        _setupBaseLayer();
-        _setupStationsLayer();
-        _setupLayersControl();
-        return map;
-    };
+  const setupMap = function () {
+    map = L.map('mapid');
+    const vp = enhydris.mapViewport;
+    map.fitBounds([[vp[1], vp[0]], [vp[3], vp[2]]]);
+    L.control.scale().addTo(map);
+    L.control.mousePosition(
+      { position: 'bottomright', emptyString: '' },
+    ).addTo(map);
+    return map;
+  };
 
-    var _setupMap = function () {
-        map = L.map("mapid");
-        var vp = enhydris.mapViewport;
-        map.fitBounds([[vp[1], vp[0]], [vp[3], vp[2]]]);
-        L.control.scale().addTo(map);
-        L.control.mousePosition(
-            {"position": "bottomright", "emptyString": ""}
-        ).addTo(map);
-        return map;
-    };
+  const setupBaseLayer = function () {
+    enhydris.mapBaseLayers[enhydris.mapDefaultBaseLayer].addTo(map);
+  };
 
-    var _setupBaseLayer = function () {
-        enhydris.mapBaseLayers[enhydris.mapDefaultBaseLayer].addTo(map);
-    };
+  const setupStationsLayer = function () {
+    let url = `${enhydris.rootUrl}stations/kml/`;
+    if (enhydris.mapMode === 'many-stations') url += `?q=${enhydris.searchString}`;
+    else if (enhydris.mapMode === 'single-station') url += `?gentity_id=${enhydris.agentityId}`;
+    stationsLayer = new L.KML(url, { async: true });
+    map.addLayer(stationsLayer);
+  };
 
-    var _setupStationsLayer = function () {
-        var url = enhydris.rootUrl + "stations/kml/"
-        if(enhydris.mapMode == "many-stations")
-            url += "?q=" + enhydris.searchString
-        else if(enhydris.mapMode == "single-station")
-            url += "?gentity_id=" + enhydris.agentityId;
-        stationsLayer = new L.KML(url, {async: true});
-        map.addLayer(stationsLayer);
-    };
+  const setupLayersControl = function () {
+    L.control.layers(enhydris.mapBaseLayers, { Stations: stationsLayer }).addTo(map);
+  };
 
-    var _setupLayersControl = function () {
-        L.control.layers(enhydris.mapBaseLayers, {"Stations": stationsLayer}).addTo(map);
-    };
+  const init = function () {
+    setupMap();
+    setupBaseLayer();
+    setupStationsLayer();
+    setupLayersControl();
+    return map;
+  };
 
-    var listStationsVisibleOnMap = function () {
-        var queryParams = Arg.all();
-        queryParams.bbox = map.getBounds().toBBoxString();
-        var queryString = '';
-        for (var param in queryParams) {
-            if (queryParams.hasOwnProperty(param)) {
-                if (param === '') {
-                    continue;
-                }
-                if (queryString) {
-                    queryString += '&';
-                }
-                queryString += param + '=' + queryParams[param];
-            }
+  const listStationsVisibleOnMap = function () {
+    const queryParams = Arg.all(); /* eslint no-undef: "off" */
+    queryParams.bbox = map.getBounds().toBBoxString();
+    let queryString = '';
+    Object.keys(queryParams).forEach((param) => {
+      if (Object.prototype.hasOwnProperty.call(queryParams, param)) {
+        if (param !== '') {
+          if (queryString) {
+            queryString += '&';
+          }
+          queryString += `${param}=${queryParams[param]}`;
         }
-        window.location = window.location.pathname + '?' + queryString;
-    };
+      }
+    });
+    window.location = `${window.location.pathname}?${queryString}`;
+  };
 
-    return {
-        init: init,
-        listStationsVisibleOnMap: listStationsVisibleOnMap,
-        setupMap: _setupMap,
-        setupBaseLayer: _setupBaseLayer,
-    };
+  return {
+    init,
+    listStationsVisibleOnMap,
+    setupMap,
+    setupBaseLayer,
+  };
 }());
 
-
 enhydris.coordinatesUI = (function namespace() {
-    'use strict';
-    /* To determine the location of a station, the user must set the
+  /* To determine the location of a station, the user must set the
     abscissa , the ordinate and the id of the reference system ,
     information very specialized and incomprehensible to most users
     who just know the longitude and latitude of the station.
@@ -78,72 +73,72 @@ enhydris.coordinatesUI = (function namespace() {
     this.
     */
 
-    var transText = enhydris.transCoordinatesUI;
+  const transText = enhydris.transCoordinatesUI;
 
-    var switchToSimpleView = function () {
-        // Hide elements for simple view
-        var elementIDArray = ['id_srid'];
-        for (var i = 0; i < elementIDArray.length; i++) {
-            $('#' + elementIDArray[i]).hide();
-            $('label[for="' + elementIDArray[i] + '"]').hide();
-        }
-        // Prepare switch for the next view: advanced
-        $('#btnCoordinates').html(transText.btnAdvanceView);
-        $('#btnCoordinates').attr('form-view', 'advanced');
-        // Alter elements attributes
-        $('label[for="id_srid"]').text(transText.formSridLabel);
-        $('label[for="id_abscissa"]').text(transText.formLongtitudeLabel);
-        $('#id_abscissa').attr('placeholder', 'ex. 20.94546');
-        $('label[for="id_ordinate"]').text(transText.formLatitudeLabel);
-        $('#id_ordinate').attr('placeholder', 'ex. 39.12171');
-    };
+  const switchToSimpleView = function () {
+    // Hide elements for simple view
+    const elementIDArray = ['id_srid'];
+    for (let i = 0; i < elementIDArray.length; i += 1) {
+      $(`#${elementIDArray[i]}`).hide();
+      $(`label[for="${elementIDArray[i]}"]`).hide();
+    }
+    // Prepare switch for the next view: advanced
+    $('#btnCoordinates').html(transText.btnAdvanceView);
+    $('#btnCoordinates').attr('form-view', 'advanced');
+    // Alter elements attributes
+    $('label[for="id_srid"]').text(transText.formSridLabel);
+    $('label[for="id_abscissa"]').text(transText.formLongtitudeLabel);
+    $('#id_abscissa').attr('placeholder', 'ex. 20.94546');
+    $('label[for="id_ordinate"]').text(transText.formLatitudeLabel);
+    $('#id_ordinate').attr('placeholder', 'ex. 39.12171');
+  };
 
-    var switchToAdvancedView = function () {
-        // Show elements for advanced view
-        var elementIDArray = ['id_srid'];
-        for (var i = 0; i < elementIDArray.length; i++) {
-            $('#' + elementIDArray[i]).show();
-            $('label[for="' + elementIDArray[i] + '"]').show();
-        }
-        // Prepare  switch for the next view: simple
-        $('#btnCoordinates').html(transText.btnSimpleView);
-        $('#btnCoordinates').attr('form-view', 'simple');
-        // Alter elements attributes
-        $('label[for="id_srid"]').text(transText.formSridLabel);
-        $('label[for="id_abscissa"]').text(transText.formAbscissaLabel);
-        $('#id_abscissa').attr('placeholder', 'ex. 20.94546');
-        $('label[for="id_ordinate"]').text(transText.formOrdinateLabel);
-        $('#id_ordinate').attr('placeholder', 'ex. 39.12171');
-    };
+  const switchToAdvancedView = function () {
+    // Show elements for advanced view
+    const elementIDArray = ['id_srid'];
+    for (let i = 0; i < elementIDArray.length; i += 1) {
+      $(`#${elementIDArray[i]}`).show();
+      $(`label[for="${elementIDArray[i]}"]`).show();
+    }
+    // Prepare  switch for the next view: simple
+    $('#btnCoordinates').html(transText.btnSimpleView);
+    $('#btnCoordinates').attr('form-view', 'simple');
+    // Alter elements attributes
+    $('label[for="id_srid"]').text(transText.formSridLabel);
+    $('label[for="id_abscissa"]').text(transText.formAbscissaLabel);
+    $('#id_abscissa').attr('placeholder', 'ex. 20.94546');
+    $('label[for="id_ordinate"]').text(transText.formOrdinateLabel);
+    $('#id_ordinate').attr('placeholder', 'ex. 39.12171');
+  };
 
-    var toggleCoordinatesView = function () {
-        // Main Switch, handles on click in btn id='btnCoordinates'
-        var viewCase = $('#btnCoordinates').attr('form-view');
-        if (viewCase === 'simple') {
-            switchToSimpleView();
-        }
-        if (viewCase === 'advanced') {
-            switchToAdvancedView();
-        }
-    };
+  const toggleCoordinatesView = function () {
+    // Main Switch, handles on click in btn id='btnCoordinates'
+    const viewCase = $('#btnCoordinates').attr('form-view');
+    if (viewCase === 'simple') {
+      switchToSimpleView();
+    }
+    if (viewCase === 'advanced') {
+      switchToAdvancedView();
+    }
+  };
 
-    var initializeCoordinatesView = function () {
-        // if SRID is '' or null then '4326'
-        // This is equal to say 'SRID' default value equals '4326'
-        if ($('#id_srid').val() === '' || $('#id_srid').val() === null) {
-            $('#id_srid').attr('value', '4326');
-        }
-        var sridValue = $('#id_srid').val();
-        if (sridValue === '4326') {
-            switchToSimpleView();
-            $('#btnCoordinates').show();
-        } else {
-            $('#btnCoordinates').hide();
-            switchToAdvancedView();
-        }
-    };
-    return {
-        initializeCoordinatesView: initializeCoordinatesView,
-        toggleCoordinatesView: toggleCoordinatesView
-    };
+  const initializeCoordinatesView = function () {
+    // if SRID is '' or null then '4326'
+    // This is equal to say 'SRID' default value equals '4326'
+    if ($('#id_srid').val() === '' || $('#id_srid').val() === null) {
+      $('#id_srid').attr('value', '4326');
+    }
+    const sridValue = $('#id_srid').val();
+    if (sridValue === '4326') {
+      switchToSimpleView();
+      $('#btnCoordinates').show();
+    } else {
+      $('#btnCoordinates').hide();
+      switchToAdvancedView();
+    }
+  };
+  return {
+    initializeCoordinatesView,
+    toggleCoordinatesView,
+  };
 }());
